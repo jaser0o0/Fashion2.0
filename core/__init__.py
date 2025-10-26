b4436ffdae3626aa7e91ff5fbbb97c9b21e20e7e
Jaser Ashkan <jma6900@psu.edu>
2025-10-26 08:13:57 -0400

    Delete core directory

--- core/__init__.py
+++
@@ -1,3 +0,0 @@
-# optional, but helps Python treat this as a package
-# you can list public modules if you want, but not required:
-# __all__ = ["analyzer", "feedback", "queryhandler", "recommender", "scraper", "shapedetector", "storage"]
